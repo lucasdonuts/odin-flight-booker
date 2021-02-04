df17c2a9459edbf93ea4c376ab5da0d3309e4e37
Lucas <lucasdonuts@gmail.com>
2021-02-03 19:02:48 -0600

    Set Up Email Settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,8 +64,6 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: 'https://odin-lucas-flight-booker.herokuapp.com/' }
-
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
@@ -119,4 +117,18 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  # Email config settings for Heroku deployment
+
+  config.action_mailer.default_url_options = { host: 'odin-lucas-flight-booker.herokuapp.com', protocol: 'https' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.mail.yahoo.com',
+    port: 587,
+    authentication: :login,
+    user_name: ENV['YAHOO_USERNAME'],
+    password: ENV['YAHOO_PASSWORD']
+  }
+
 end
